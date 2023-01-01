756f66ba42f3135e06ae1ecd4ba3b82210441889
Youcef-Abdelliche <hy_abdelliche@esi.dz>
2023-01-01 14:50:25 +0100

    create models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import User
+from .models import Admin, Doctor, Patient, Specialization, User, UserProfile
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Admin)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Specialization)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,53 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    USER_TYPE_CHOICES = ((1, "admin"), (2, "doctor"), (3, "patient"))
+
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, default=2)
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class UserProfile(models.Model):
+    SEX_CHOICES = (("M", "male"), ("F", "female"))
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=14)
+    # image
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sex = models.CharField(default="M", choices=SEX_CHOICES, max_length=1)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Admin(models.Model):
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user_profile.first_name} {self.user_profile.last_name}"
+
+
+class Doctor(models.Model):
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    specialization = models.ForeignKey(Specialization, on_delete=models.PROTECT)
+    address = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.user_profile.first_name} {self.user_profile.last_name}"
+
+
+class Patient(models.Model):
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user_profile.first_name} {self.user_profile.last_name}"
